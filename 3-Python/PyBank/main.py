1b77f064b18d4fdbc55dca44ef093b01d08e119c
Matt Davies <matt.davies1@mac.com>
2020-10-16 19:27:31 -0400

    Upload Python Homework

---
+++ 3-Python/PyBank/main.py
@@ -0,0 +1,47 @@
+# Import dependancies
+import os
+import csv
+from statistics import mean
+
+# Set filepath
+csv_filepath = os.path.join("Resources", "budget_data.csv")
+
+# establish variables
+total_months = 0
+net_profit = 0
+average_change = []
+profit_months = []
+profit_values = []
+
+# Open csv
+with open(csv_filepath) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    header = next(csv_file)                 # skip header
+    for row in csv_reader:
+        total_months += 1                   # The total number of months included in the dataset
+        net_profit += int(row[1])           # The net total amount of "Profit/Losses" over the entire period
+        
+        profit_months.append(row[0])        # populate lists for min/max/delta values
+        profit_values.append(int(row[1]))
+
+# Calculate month-to-month differences
+for i in range(1, len(profit_months)):
+    average_change.append(profit_values[i] - profit_values[i - 1])
+
+# Results in f-string
+output = f"""
+Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${net_profit}
+Average  Change: ${mean(average_change):.2f}
+Greatest Increase in Profits: {profit_months[profit_values.index(max(profit_values))]} (${max(profit_values)})
+Greatest Decrease in Profits: {profit_months[profit_values.index(min(profit_values))]} (${min(profit_values)})
+"""
+
+# Print outputs
+print(output)
+
+# Write outputs to .txt
+with open("Output.txt", 'w') as text_file:
+    text_file.write(output)
---
+++ 3-Python/PyPoll/main.py
@@ -0,0 +1,63 @@
+# import dependancies
+import csv
+import os
+
+# Set filepath
+data_path = os.path.join("Resources", "election_data.csv")
+
+# Establish variables for reading loop
+vote_total = 0
+candidate_list = []
+
+# Open file, read by line and output total number votes & list of candidates
+with open(data_path, 'r', newline = '') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter = ',')
+    header = next(csvreader)                    # skip header
+    for row in csvreader:
+        vote_total += 1                         # The total number of votes cast
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])       # A complete list of candidates who received votes
+
+# Make Dictionary to tally votes into by candidate
+vote_tally = dict.fromkeys(candidate_list , 0)
+
+# Tally total number of votes each candidate won
+with open(data_path, 'r', newline = '') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter = ',')
+    header = next(csvreader)                    # skip header
+    for row in csvreader:
+        for key in vote_tally:
+            if row[2] == key:
+                vote_tally[key] += 1
+
+# Calculate percentage of votes each candidate won & put values into new dictionary
+vote_pct = dict.fromkeys(candidate_list , 0)
+for key in vote_pct:
+    vote_pct[key] = (vote_tally[key] / vote_total) * 100
+
+# Generate parallel lists to pull the winner of the election based on popular vote.
+votes = list(vote_tally.values())
+candidates = list(vote_tally.keys())
+
+# Loop over dictionaries to produce f-strings for output
+results_list = []
+for name in candidate_list:
+    results_list.append(f"{name}: {vote_pct[name]:.3f}% ({vote_tally[name]})")
+vote_report = "\n".join(results_list)
+
+# Generate f-string for output
+output = f'''Election Results
+-------------------------
+Total Votes: {vote_total}
+-------------------------
+{vote_report}
+-------------------------
+# Winner: {candidates[votes.index(max(votes))]}
+# -------------------------'''
+
+# Print results in terminal
+print(output)
+
+# Write outputs to .txt
+with open("Output.txt", 'w') as text_file:
+    text_file.write(output)
