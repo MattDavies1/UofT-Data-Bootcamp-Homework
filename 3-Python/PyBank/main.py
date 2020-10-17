11349b2a12176842f5bf36c561337fee1f0e56a7
Matt Davies <matt.davies1@mac.com>
2020-10-17 14:04:43 -0400

    Finalize python homework and start Pandas

--- 3-Python/PyBank/main.py
+++ 3-Python/PyBank/main.py
@@ -6,7 +6,7 @@ from statistics import mean
 # Set filepath
 csv_filepath = os.path.join("Resources", "budget_data.csv")
 
-# establish variables
+# Establish variables
 total_months = 0
 net_profit = 0
 average_change = []
@@ -16,26 +16,26 @@ profit_values = []
 # Open csv
 with open(csv_filepath) as csv_file:
     csv_reader = csv.reader(csv_file)
-    header = next(csv_file)                 # skip header
+    header = next(csv_file)                 # Skip header
     for row in csv_reader:
-        total_months += 1                   # The total number of months included in the dataset
-        net_profit += int(row[1])           # The net total amount of "Profit/Losses" over the entire period
+        total_months += 1                   # Calculate total number of months included in the dataset
+        net_profit += int(row[1])           # Calculate net total amount of "Profit/Losses" over the entire period
         
-        profit_months.append(row[0])        # populate lists for min/max/delta values
+        profit_months.append(row[0])        # Populate lists for min/max/delta values
         profit_values.append(int(row[1]))
 
 # Calculate month-to-month differences
 for i in range(1, len(profit_months)):
     average_change.append(profit_values[i] - profit_values[i - 1])
 
-# Results in f-string
+# Format results to f-string
 output = f"""Financial Analysis
 ----------------------------
 Total Months: {total_months}
 Total: ${net_profit}
 Average  Change: ${mean(average_change):.2f}
-Greatest Increase in Profits: {profit_months[profit_values.index(max(profit_values))]} (${max(profit_values)})
-Greatest Decrease in Profits: {profit_months[profit_values.index(min(profit_values))]} (${min(profit_values)})"""
+Greatest Increase in Profits: {profit_months[average_change.index(max(average_change))+1]} (${max(average_change)})
+Greatest Decrease in Profits: {profit_months[average_change.index(min(average_change))+1]} (${min(average_change)})"""
 
 # Print outputs
 print(output)
--- 3-Python/PyPoll/main.py
+++ 3-Python/PyPoll/main.py
@@ -1,4 +1,4 @@
-# import dependancies
+# Import dependancies
 import csv
 import os
 
@@ -12,11 +12,11 @@ candidate_list = []
 # Open file, read by line and output total number votes & list of candidates
 with open(data_path, 'r', newline = '') as csv_file:
     csvreader = csv.reader(csv_file, delimiter = ',')
-    header = next(csvreader)                    # skip header
+    header = next(csvreader)                    # Skip header
     for row in csvreader:
-        vote_total += 1                         # The total number of votes cast
+        vote_total += 1                         # Calculate total number of votes cast
         if row[2] not in candidate_list:
-            candidate_list.append(row[2])       # A complete list of candidates who received votes
+            candidate_list.append(row[2])       # Generate list of candidates who received votes
 
 # Make Dictionary to tally votes into by candidate
 vote_tally = dict.fromkeys(candidate_list , 0)
@@ -52,8 +52,8 @@ Total Votes: {vote_total}
 -------------------------
 {vote_report}
 -------------------------
-# Winner: {candidates[votes.index(max(votes))]}
-# -------------------------'''
+Winner: {candidates[votes.index(max(votes))]}
+-------------------------'''
 
 # Print results in terminal
 print(output)
