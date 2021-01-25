ff1824f9524b5260348a8d6506ab05b756198e4b
Matt Davies <matt.davies1@mac.com>
2021-01-25 18:34:18 -0500

    add listener

--- 12-Javascript/UFO-level-1/static/js/app.js
+++ 12-Javascript/UFO-level-1/static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
+// Arrow function to add every variable to the table
 data.forEach((sighting) => {
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
@@ -11,3 +12,9 @@ data.forEach((sighting) => {
       cell.text(value);
     });
   });
+
+// Add listener for search function
+d3.selectAll("#button").on("click", filterFunc)
+
+// function to filter table
+function filterFunc() {}
