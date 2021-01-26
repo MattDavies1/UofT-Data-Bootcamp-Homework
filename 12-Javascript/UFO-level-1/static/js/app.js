0779b56537b5fc9576692bbafe2b4c057e765eb4
Matt Davies <matt.davies1@mac.com>
2021-01-25 20:48:09 -0500

    update table search and replace function

--- 12-Javascript/UFO-level-1/static/js/app.js
+++ 12-Javascript/UFO-level-1/static/js/app.js
@@ -1,10 +1,48 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select button
+var button = d3.select("#filter-btn");
+
+// select form
+var form = d3.select("#form");
+
+// select table body for data insert
 var tbody = d3.select("tbody");
 
-// Arrow function to add every variable to the table
+// event handelers
+button.on("click", filterFunc)
+form.on("submit", filterFunc)
+
+// function to filter table
+function filterFunc() {
+
+    // prevend auto-refresh
+    d3.event.preventDefault();
+
+    // accept value from input feild
+    var inputVal = d3.select("#datetime").node().value
+
+    // create filtered data
+    var filteredData = tableData.filter(event => event.datetime == inputVal)
+
+    // clear data in table
+    d3.selectAll("td").remove()
+
+    // insert filtered data
+    filteredData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+    // clear input feild
+    d3.select("#datetime").node().value = ""
+};
+
+// init function to add every variable to the table on page load
 data.forEach((sighting) => {
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
@@ -12,9 +50,3 @@ data.forEach((sighting) => {
       cell.text(value);
     });
   });
-
-// Add listener for search function
-d3.selectAll("#button").on("click", filterFunc)
-
-// function to filter table
-function filterFunc() {}
